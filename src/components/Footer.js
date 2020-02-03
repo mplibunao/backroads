50fb92378e92735a2f1a56877a707edebdd1bf60
MP <markpaololibunao@gmail.com>
2020-02-04 05:12:20 +0800

    Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import styles from "../css/footer.module.css"
 
 export default () => (
-    <div>
-    Footer<Link to="/blog/">blog page</Link>
+  <footer className={styles.footer}>
+    <div className={styles.links}>
+      {links.map((item, index) => {
+        return (
+          <Link key={index} to={item.path}>
+            {item.text}
+          </Link>
+        )
+      })}
     </div>
+    <div className={styles.icons}>
+      {socialIcons.map((item, index) => {
+        return (
+          <a
+            key={index}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.icon}
+          </a>
+        )
+      })}
+    </div>
+    <div className={styles.copyright}>
+      copyright &copy; backroads travel company {new Date().getFullYear()} all
+      rights reserved
+    </div>
+  </footer>
 )
