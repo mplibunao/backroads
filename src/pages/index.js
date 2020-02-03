90129bc4fc7dbea5cf9ec4e334e7d5f5d3ef3795
MP <markpaololibunao@gmail.com>
2020-02-04 07:17:36 +0800

    Finishes About Us and Services section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
+import About from "../components/Home/About"
+import Services from "../components/Home/Services"
 
 export default () => (
   <>
@@ -17,6 +19,8 @@ export default () => (
           </Link>
         </Banner>
       </SimpleHero>
+      <About />
+      <Services />
     </Layout>
   </>
 )
