46ef0ff585c01188fb140aed9d643f927f7a8b20
MP <markpaololibunao@gmail.com>
2020-02-04 05:13:03 +0800

    Creates working hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 
 export default () => (
   <>
     <Layout>
-      Hello world!
+      <SimpleHero>
+        <Banner
+          title="continue exploring"
+          info=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget"
+        >
+          <Link to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </SimpleHero>
     </Layout>
   </>
 )
