0a04d8dcddd6676eba69a5a4fdbe373d09e64dd2
MP <markpaololibunao@gmail.com>
2020-02-05 15:19:06 +0800

    Uses gatsby-image plugin for about us image with svg tracer

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,16 +1,30 @@
 import React from "react"
 import Title from "../Title"
 import styles from "../../css/about.module.css"
-import img from "../../images/defaultBcg.jpeg"
+import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
+
+const getAbout = graphql`
+  query aboutImage {
+    aboutImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 const About = () => {
+  const { aboutImage } = useStaticQuery(getAbout)
   return (
     <section className={styles.about}>
       <Title title="about" subtitle="us" />
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
           <div className={styles.imgContainer}>
-            <img src={img} alt="about company" />
+            <Img fluid={aboutImage.childImageSharp.fluid} alt="awesome landscape" />
           </div>
         </article>
         <article className={styles.aboutInfo}>
@@ -23,7 +37,9 @@ const About = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
             pellentesque luctus purus
           </p>
-      <button type='button' className='btn-primary'>read more</button>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
         </article>
       </div>
     </section>
