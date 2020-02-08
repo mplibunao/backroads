517eba30713b3b77e61116de66a732b073adec78
MP <markpaololibunao@gmail.com>
2020-02-08 14:36:19 +0800

    Adds script for pre-optimizing images using sharp for overall better build times

---
+++ src/scripts/optimize.js
@@ -0,0 +1,32 @@
+const sharp = require(`sharp`)
+const glob = require(`glob`)
+const fs = require(`fs-extra`)
+// const matches = glob.sync(`src/images/tour-images/**/*.{png,jpg,jpeg}`)
+const matches = glob.sync(`tour-images/**/*.{png,jpg,jpeg,JPG}`)
+const MAX_WIDTH = 1280
+const MAX_HEIGHT = 720
+const QUALITY = 70
+
+Promise.all(
+  matches.map(async match => {
+    const stream = sharp(match)
+    const info = await stream.metadata()
+    if (info.width < MAX_WIDTH) {
+      return
+    }
+    const optimizedName = match.replace(
+      /(\..+)$/,
+      (match, ext) => `-optimized${ext}`
+    )
+    await stream
+      .rotate()
+      .resize({
+        width: MAX_WIDTH,
+        height: MAX_HEIGHT,
+        position: 'center',
+      })
+      .jpeg({ quality: QUALITY })
+      .toFile(optimizedName)
+    return fs.rename(optimizedName, match)
+  })
+)
