e6b40435b9e672a77481943023e189c6c158cbfc
MP <markpaololibunao@gmail.com>
2020-09-27 15:49:55 +0800

    Allows rendering of other blog entry as part of rich text

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import styles from "../css/single-blog.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 const Blog = ({ data }) => {
   const {
@@ -14,27 +14,37 @@ const Blog = ({ data }) => {
   const options = {
     renderNode: {
       "embedded-asset-block": node => {
-        return <div className='rich'>
-          <h3>this is awesome image</h3>
-          <img width="400" src={node.data.target.fields.file['en-US'].url} />
-          <p>images provided by john doe</p>
-        </div>
+        return (
+          <div className="rich">
+            <h3>this is awesome image</h3>
+            <img width="400" src={node.data.target.fields.file["en-US"].url} />
+            <p>images provided by john doe</p>
+          </div>
+        )
       },
       "embedded-entry-block": node => {
-        console.log(node.data.target.fields)
-        return <div>
-          <br />
-          <br />
-          <br />
-          <br />
-          <h1>this is other post</h1>
-          <br />
-          <br />
-          <br />
-          <br />
+        const { title, image, text } = node.data.target.fields
+        return (
+          <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <h1>this is other post: {title["en-US"]}</h1>
+            <img
+              width="400"
+              src={image["en-US"].fields.file["en-US"].url}
+              alt=""
+            />
+            {documentToReactComponents(text["en-US"])}
+            <br />
+            <br />
+            <br />
+            <br />
           </div>
-      }
-    }
+        )
+      },
+    },
   }
   return (
     <Layout>
@@ -42,7 +52,9 @@ const Blog = ({ data }) => {
         <div className={styles.center}>
           <h1>{title}</h1>
           <h4>published at: {published}</h4>
-          <article className={styles.post}>{documentToReactComponents(json, options)}</article>
+          <article className={styles.post}>
+            {documentToReactComponents(json, options)}
+          </article>
           <AniLink fade to="/blog" className="btn-primary">
             all posts
           </AniLink>
