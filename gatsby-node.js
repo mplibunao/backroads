4abf7259cd95959edab2e34a9c69be14268d2051
MP <markpaololibunao@gmail.com>
2020-09-29 09:12:34 +0800

    Adds pagination to /blogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,4 +42,24 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       },
     })
   })
+
+  // amount of posts
+  const posts = result.data.posts.edges
+  // posts per page
+  const postsPerPage = 3
+  // how many pages
+  const numPages = Math.ceil(posts.length / postsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? "/blogs" : `/blogs/${i + 1}`,
+      component: path.resolve("./src/templates/blog-list-template.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
+  })
 }
